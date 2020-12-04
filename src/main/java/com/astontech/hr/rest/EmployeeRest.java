566d24d2015f17f273dfa1737b9d941823c275bf
Jaden-Reklaw <jwalker117@gmail.com>
2020-12-04 13:27:44 -0600

    created rest for post and delete and tested using postman

--- src/main/java/com/astontech/hr/rest/EmployeeRest.java
+++ src/main/java/com/astontech/hr/rest/EmployeeRest.java
@@ -2,25 +2,47 @@ package com.astontech.hr.rest;
 
 import com.astontech.hr.domain.Employee;
 import com.astontech.hr.services.EmployeeService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/employee/")
 public class EmployeeRest {
+
+    private Logger log = Logger.getLogger(EmployeeRest.class);
+
     @Autowired
     private EmployeeService employeeService;
 
+    //Returns all employees
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public Iterable<Employee> getAll() {
         return employeeService.listAllEmployees();
     }
 
+    //Returns employee based on ID
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Employee getById(@PathVariable int id) {
         return employeeService.getEmployeeByID(id);
+    }
+
+    //Post a new employee to the database
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public Employee save(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    //Deletes and employee based on ID
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public Boolean delete(@PathVariable int id) {
+        boolean result = false;
+        try {
+            employeeService.deleteEmployee(id);
+            result = true;
+        } catch (Exception ex) {
+            log.info(ex);
+        }
+        return result;
     }
 }
